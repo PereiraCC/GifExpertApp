2a4be8bb3951056f887fd1469f16105d56bd34c0
Carlos Pereira <carloscoto0103@gmail.com>
2021-10-30 11:40:08 -0600

    helper getGifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 
@@ -7,28 +8,9 @@ export const GifGrid = ( { category } ) => {
     const [images, setImages] = useState([]);
 
     useEffect( () => {
-        getGifs();
-    }, [])
-
-    const getGifs = async () => {
-
-        const url = 'https://api.giphy.com/v1/gifs/search?q=Apple&limit=10&api_key=Jd1e5klekNlv3DuGGIcbYbJGkmJ6xXyv';
-        const resp = await fetch( url );
-        const { data } = await resp.json();
-
-        const gifs = data.map( img => {
-            return {
-                id: img.id,
-                title : img.title,
-                url: img.images?.downsized_medium.url
-            }
-        });
-
-        // console.log(gifs);
-        setImages( gifs );
-    };
-
-    // getGifs();
+        getGifs( category )
+            .then( setImages )
+    }, [ category ])
 
     return (
         <>
