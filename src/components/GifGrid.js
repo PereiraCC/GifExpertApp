3edfc2c4cf1da821f3b932ac77d17fada2ba1a86
Carlos Pereira <carloscoto0103@gmail.com>
2021-10-30 11:19:06 -0600

    Styles added

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -33,15 +33,17 @@ export const GifGrid = ( { category } ) => {
     return (
         <>
             <h3> { category } </h3>   
+            <div className="card-grid">
+                {
+                    images.map( ( img ) => (
+                        <GifGridItem 
+                            key= {img.id}
+                            { ...img }
+                        />
+                    ))
+                }
+            </div>
 
-            {
-                images.map( ( img ) => (
-                    <GifGridItem 
-                        key= {img.id}
-                        { ...img }
-                    />
-                ))
-            }
         </>
     )
 }
